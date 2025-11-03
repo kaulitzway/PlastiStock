594dddde51c00b518d3949f22b6c693125fe5946
Kaulitzway <ramirezllamasraul@gmail.com>
2025-11-02 20:13:45 -0500

    Primer commit del proyecto PlastiStock

---
+++ PlastiStock/Context/PlastiStockContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlastiStock.Models;
+
+namespace PlastiStock.Contest
+{
+    public class PlasticStockContext : DbContext
+    {
+        public PlasticStockContext(DbContextOptions<PlasticStockContext> options)
+            : base(options)
+        {
+        }
+
+        // Estas son las tablas (entidades)
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<TipoDocumento> TiposDeDocumento { get; set; }
+
+        // Configuraciones adicionales
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            // Relación uno a muchos entre TipoDocumento y Usuario
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey (e => e.Id);
+                entity.Property(e => e.Nombre).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Apellido).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Correo).IsRequired().HasMaxLength(150);
+                entity.Property(e => e.Contraseña).IsRequired().HasMaxLength(255);
+                entity.HasOne(e => e.TipoDocumento)
+                      .WithMany(t => t.Usuarios)
+                      .HasForeignKey(e => e.TipoDocumentoId);
+
+
+
+            } );
+
+        }
+
+
+    }
+
+}
+
---
+++ PlastiStock/Models/user.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Identity.Client;
+
+namespace PlastiStock.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Key]
+        public int TipoDocumentoId { get; set; }  // Clave foránea
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+        // Relación con TipoDocumento
+        public TipoDocumento TipoDocumento { get; set; }
+    }
+}
+
+    
+
